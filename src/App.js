edff2842a80270cf7951a79ca1d239938164eb9d
CjBoat2542 <55519892+CjBoat2542@users.noreply.github.com>
2019-09-21 14:17:12 +0700

    gamelogic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard'
-const word = "Hello";
+const word = ['Hello', 'Green'];
+var item = word[Math.floor(Math.random()*word.length)];
 
 class App extends Component {
   render() {
   return (
-    <div>
- <WordCard value="CJBoat"/>
+    <div className = 'App'>
+ <WordCard value={item.toUpperCase()}/>
+ <h2 id="nod">Hello World!</h2>
  </div>
  );
   }
