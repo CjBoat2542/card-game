9c1a81d4742ec0f662554e55834ca1126abdb449
CjBoat2542 <55519892+CjBoat2542@users.noreply.github.com>
2019-09-21 14:02:35 +0700

    labreact

---
+++ src/CharacterCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class CharacterCard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        active: false,
+        }   
+    }  
+    activate = () => {
+        if(!this.state.active){
+            this.props.activationHandler(this.props.value)
+            this.setState({active: true})
+        }
+        else{
+            this.setState({active: false})
+        }
+        }    
+ render(){
+    let className = `card ${this.state.active ? 'activeCard': ''}`
+ return (
+<div className={className} onClick={this.activate}>
+ {this.props.value}
+ </div>
+ )
+ }
+}
